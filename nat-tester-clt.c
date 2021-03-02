e78ef8eaad7990b65e96845733346cc93730064a
Willian B <speedyb0y@users.noreply.github.com>
2021-03-02 01:52:14 -0300

    Update nat-tester-clt.c

--- nat-tester-clt.c
+++ nat-tester-clt.c
@@ -52,6 +52,20 @@ static uint str2ip (const char* const str) {
 
 int main (int argsN, char* args[]) {
 
+    char buff[1400] =
+        "e32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgk"
+        "jdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggew"
+        "Egjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghre"
+        "ioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t765"
+        "7kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@"
+        "we#@@4ergaqw54t7657kuymhg;dfweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!"
+        "23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32"
+        "453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539"
+        "809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlke"
+        "g9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoig"
+        "hweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gw564864640436774eggreg25e321!!23tge23tgewwgegge@we"
+        ;
+
     if (argsN != 6)
         return 1;
 
@@ -61,48 +75,47 @@ int main (int argsN, char* args[]) {
     const uint connsN  = atoi(args[4]);
     const uint pktsN   = atoi(args[5]);
 
-    u16 conns[connsN];
+    u16 conns[connsN]; uint errors = 0;
 
     SockAddrIP4 cltAddr = { .sin_family = AF_INET, .sin_port = 0,       .sin_addr = { .s_addr = cltIP } };
     SockAddrIP4 srvAddr = { .sin_family = AF_INET, .sin_port = srvPort, .sin_addr = { .s_addr = srvIP } };
 
+    printf("CONNECTING SOCKETS\n");
+
     const int opt = 1;
 
-    uint i = 0;
+    for (uint i = 0; i != connsN; i++) {
 
-    while (i != connsN) {
+        if (i % 150 == 0)
+            sleep(1);
 
-        const int sock = socket(AF_INET, SOCK_STREAM, 0);
+        const int sock = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
 
-        if (sock != -1) {
-            if (setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) == 0 &&
-                bind(sock, (SockAddr*)&cltAddr, sizeof(SockAddrIP4)) == 0 &&
-                connect(sock, (SockAddr*)&srvAddr, sizeof(SockAddrIP4)) == 0) {
-                conns[i++] = (uint)sock;
-            } else
-                close(sock);
-        }
+        if (sock <= 0 ||
+            setsockopt(sock, IPPROTO_TCP, TCP_NODELAY, &opt, sizeof(opt)) ||
+            bind(sock, (SockAddr*)&cltAddr, sizeof(SockAddrIP4)) || // TODO: ONLY IF cltIP
+            connect(sock, (SockAddr*)&srvAddr, sizeof(SockAddrIP4)) != -1 ||
+            errno != EINPROGRESS)
+            abort();
+
+        conns[i] = (uint)sock;
     }
+
+    printf("WAITING CONNECTIONS\n");
+
+    for (uint i = 0; i != connsN; i++)
+        if (fcntl(conns[i], F_SETFL, fcntl(conns[i], F_GETFL, 0) & ~O_NONBLOCK) ||
+            connect(conns[i], (SockAddr*)&srvAddr, sizeof(SockAddrIP4))) {
+            close(conns[i]);
+            conns[i] = 0;
+            errors++;
+        }
 
     printf("TRANSFERRING\n");
 
-    char buff[1400] =
-        "e32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgk"
-        "jdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggew"
-        "Egjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghre"
-        "ioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t765"
-        "7kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@"
-        "we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!"
-        "23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32"
-        "453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlkeg9043890t347890539"
-        "809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoighweooyihnfgkjdjlke"
-        "g9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we#@@4ergaqw54t7657kuymhg;dfwekjghreioht5r9jIewgweggewEgjwoig"
-        "hweooyihnfgkjdjlkeg9043890t347890539809(*&*(*@$*(ewe32453425gww325e321!!23tge23tgewwgegge@we"
-        ;
+    for (uint count = 0; count != pktsN; count++) {
 
-    uint errors = 0;
-
-    for (uint count = pktsN; count; count--) {
+        printf("#%u WRITE \n", count);
 
         for (uint i = 0; i != connsN; i++)
             if (conns[i])
@@ -112,19 +125,24 @@ int main (int argsN, char* args[]) {
                     errors++;
                 }
 
+        printf("#%u READ \n", count);
+
         for (uint i = 0; i != connsN; i++)
             if (conns[i])
-                if (read (conns[i], buff, sizeof(buff)) != sizeof(buff)) {
+                if (read(conns[i], buff, sizeof(buff)) != sizeof(buff)) {
                     close(conns[i]);
                     conns[i] = 0;
                     errors++;
                 }
+
+        printf("\n");
     }
 
     printf("ERRORS: %u\n", errors);
 
     // TODO: FIXME: SALVAR NUMERO DE PACOTES, BANDWIDTH, TEMPOS TOTAIS
     // TESTAR O TIMEOUT/KEEP ALIVE, VAI AUMENTANDO O TEMPO ENTRE OS ENVIOS
+    // TODO: FIXME: CONTAR SEPARADAMENTE OS ERROS SZEOF() DE CONEXÃO
 
     return 0;
 }
