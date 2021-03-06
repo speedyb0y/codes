1b34c783a0ca541366da2e6f1a8982bcaae2d5e4
Willian B <speedyb0y@users.noreply.github.com>
2021-03-06 15:55:00 -0300

    Update proc-net-dev-mon.py

--- proc-net-dev-mon.py
+++ proc-net-dev-mon.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 import os
 import time
 
@@ -12,7 +14,7 @@ ITFCS = []
 
 lasts = [time.clock_gettime(time.CLOCK_BOOTTIME)]
 
-fields0, values0, fields1, values1, _ = (line.split()[1:] for line in os.pread(nsFD, 65536, 0).decode().split('\n'))
+fields0, values0, fields1, values1, _ = (line.split()[1:] for line in os.pread(nsFD, 65536, 0).split(b'\n'))
 
 FIELDS.extend(fields0)
 FIELDS.extend(fields1)
@@ -20,9 +22,9 @@ FIELDS.extend(fields1)
 lasts.extend(values0)
 lasts.extend(values1)
 
-for itfc, *values0 in (x.split() for x in os.pread(devFD, 65536, 0)[:-1].decode().split('\n')[2:]):
+for itfc, *values0 in (x.split() for x in os.pread(devFD, 65536, 0)[:-1].split(b'\n')[2:]):
 
-    assert itfc.endswith(':') and len(values0) == 16
+    assert itfc.endswith(b':') and len(values0) == 16
 
     ITFCS.append(itfc)
     lasts.extend(values0)
@@ -30,8 +32,8 @@ for itfc, *values0 in (x.split() for x in os.pread(devFD, 65536, 0)[:-1].decode(
 lasts = tuple(map(int, lasts))
 
 os.write(1, int(time.time()).to_bytes(8, 'little', signed=False))
-os.write(1, '|'.join(FIELDS).encode() + b'\x00')
-os.write(1, '|'.join(itfc[:-1] for itfc in ITFCS).encode() + b'\x00')
+os.write(1, b'\x01'.join(FIELDS) + b'\x00')
+os.write(1, b'\x01'.join(itfc[:-1] for itfc in ITFCS) + b'\x00')
 os.write(1, b''.join((int(x).to_bytes(8, 'little', signed=False) for x in lasts)))
 
 FIELDS.extend(ITFCS)
@@ -41,7 +43,7 @@ while True:
     fields = []
     values = [time.clock_gettime(time.CLOCK_BOOTTIME)]
 
-    fields0, values0, fields1, values1, _ = (line.split()[1:] for line in os.pread(nsFD, 65536, 0).decode().split('\n'))
+    fields0, values0, fields1, values1, _ = (line.split()[1:] for line in os.pread(nsFD, 65536, 0).split(b'\n'))
 
     fields.extend(fields0)
     fields.extend(fields1)
@@ -49,7 +51,7 @@ while True:
     values.extend(values0)
     values.extend(values1)
 
-    for itfc, *values0 in (x.split() for x in os.pread(devFD, 65536, 0)[:-1].decode().split('\n')[2:]):
+    for itfc, *values0 in (x.split() for x in os.pread(devFD, 65536, 0)[:-1].split(b'\n')[2:]):
         fields.append(itfc)
         values.extend(values0)
 
