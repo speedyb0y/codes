1b66149f99e6a515ba2eedd68e654c028bbfa82c
Willian B <speedyb0y@users.noreply.github.com>
2021-03-07 23:48:07 -0300

    Update download-file.py

--- download-file.py
+++ download-file.py
@@ -6,54 +6,73 @@ from random import choice as CHOOSE
 import socket
 
 KNOWNS = '''
-./download-file.py 8080 ping.online.net 80 /5000Mo.dat
-./download-file.py 8080 ipv4.download.thinkbroadband.com 80 /1GB.zip
-./download-file.py 8080 187.108.112.23 80 /testebw/file2.test
-./download-file.py 8080 www.telmaxbr.net.br 80 /download/dwl/702MEGA.zip
-./download-file.py 8080 ovh.net 80 /files/10Gio.dat
-./download-file.py 8080 speed.hetzner.de 80 /10GB.bin
-./download-file.py 8080 speedtest-nyc1.digitalocean.com 80 /5gb.test
+./download-file.py - ping.online.net                  80 5000Mo.dat
+./download-file.py - ipv4.download.thinkbroadband.com 80 1GB.zip
+./download-file.py - 187.108.112.23                   80 testebw/file2.test
+./download-file.py - www.telmaxbr.net.br              80 download/dwl/702MEGA.zip
+./download-file.py - ovh.net                          80 files/10Gio.dat
+./download-file.py - speed.hetzner.de                 80 10GB.bin
+./download-file.py - speedtest-nyc1.digitalocean.com  80 5gb.test
+./download-file.py - speedtest.tpa.hivelocity.net     80 10GiB.file
+./download-file.py - speedtest.nyc.hivelocity.net     80 10GiB.file
+./download-file.py - ipv4.download.thinkbroadband.com 80 1GB.zip
+./download-file.py - mirror.filearena.net             80 pub/speed/SpeedTest_2048MB.dat
+./download-file.py - speed.hetzner.de                 80 10GB.bin
+./download-file.py - speedtest-ams2.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-ams3.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-ca.turnkeyinternet.net 80 10000mb.bin
+./download-file.py - speedtest-fra1.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-lon1.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-ny.turnkeyinternet.net 80 10000mb.bin
+./download-file.py - speedtest-nyc1.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-nyc2.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-nyc3.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-sfo1.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-sfo2.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-sfo3.digitalocean.com  80 5gb.test
+./download-file.py - speedtest-tor1.digitalocean.com  80 5gb.test
+./download-file.py - speedtest.fra1.hivelocity.net    80 10GiB.file
+./download-file.py - speedtest.nyc.hivelocity.net     80 10GiB.file
+./download-file.py - speedtest.tele2.net              80 1000GB.zip
+./download-file.py - speedtest.tpa.hivelocity.net     80 10GiB.file
 '''
 
-VPNS = [f'@openvpn-{vpn}' for vpn in range(13)]
+VPNS = tuple(f'openvpn-{vpn}' for vpn in range(13))
+PROXIES = tuple(range(8080, 8089))
 
 if len(sys.argv) > 2:
-    _, PROXY, HOSTNAME, PORT, URI = sys.argv
+    _, PROXY, HOSTNAME, PORT, FILE = sys.argv
 else:
-    _, PROXY, HOSTNAME, PORT, URI = CHOOSE([line.strip().split() for line in KNOWNS.strip().split('\n')])
+    _, PROXY, HOSTNAME, PORT, FILE = CHOOSE([line.strip().split() for line in KNOWNS.strip().split('\n')])
 
-    if len(sys.argv) == 1:
-        PROXY = CHOOSE(VPNS)
+    if len(sys.argv) != 1:
+        _, PROXY = sys.argv
+
+if PROXY == '-':
+    PROXY = CHOOSE([*VPNS, *PROXIES])
+else:
+    PROXY = (PROXY[1:] if PROXY.startswith('@') else int(PROXY))
 
 # TODO: FIXME: SE NAO FOR PROXY/VPN, DEIXAR QUE SEJA IPv6
 IP = CHOOSE([ip for sockFamily, *_, (ip, *_) in socket.getaddrinfo(HOSTNAME, 443) if sockFamily == socket.AF_INET])
 
-print(PROXY, IP, PORT, HOSTNAME, URI)
+print(PROXY, IP, PORT, HOSTNAME, FILE)
 
 os.close(0)
 os.close(1)
 
 sock = socket.socket()
 
-if PROXY.startswith('@'):
-    if PROXY == '@':
-        PROXY = CHOOSE(VPNS)
-    else:
-        PROXY = PROXY[1:]
+if isinstance(PROXY, int):
+    sock.connect(('127.0.0.1', PROXY))
+    os.write(0, b''.join((b'\x04\x01', int(PORT).to_bytes(length=2, byteorder='big'), *(int(x).to_bytes(length=1, byteorder='big') for x in IP.split('.')), b'\x00')))
+    os.read(0, 65536)
+else:
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BINDTODEVICE, PROXY.encode())
     sock.connect((IP, int(PORT)))
-else:
-    sock.connect(('127.0.0.1', int(PROXY)))
 
-    # SEND PROXY CONNECT COMMAND
-    os.write(0, b''.join((b'\x04\x01', int(PORT).to_bytes(length=2, byteorder='big'), *(int(x).to_bytes(length=1, byteorder='big') for x in IP.split('.')), b'\x00')))
-
-    # WAIT CONNECTION TO BE ESTABLISHED
-    assert os.read(0, 65536)
-
-# SEND REQUEST
 os.write(0, (
-    f'GET {URI} HTTP/1.0\r\n'
+    f'GET /{FILE} HTTP/1.0\r\n'
     f'Connection: keep-alive\r\n'
     f'Host: {HOSTNAME}\r\n'
     f'\r\n'
@@ -61,7 +80,6 @@ os.write(0, (
 
 os.open('/dev/null', os.O_WRONLY)
 
-# NOW RECEIVE IT
 os.set_inheritable(0, True)
 os.set_inheritable(1, True)
 
