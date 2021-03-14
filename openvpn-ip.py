fce22ee053ac3518fbf5cf1dfff1d54ec1f276c7
Willian B <speedyb0y@users.noreply.github.com>
2021-03-14 05:31:41 -0300

    Update openvpn-ip.py

--- openvpn-ip.py
+++ openvpn-ip.py
@@ -31,13 +31,15 @@ dev = f'openvpn-{vpn}'
 
 table = 5000 + vpn
 
-cmdline, args = ' '.join(('ip', *sys.argv[1:])), sys.argv[3:]
+sys.argv[0] = 'ip'
+
+cmdline, args = ' '.join(sys.argv), sys.argv[3:]
 
 # TODO: NÃO DEIXAR SER UM DOS MEUS IPS
 # TODO: FIXME: OS GATEWAYS TAMBÉM DEVEM SER IPS PRIVADOS
 # TODO: FIXME: SÓ PODE SER IPS PRIVADOS
 
-if cmdline.startswith('ip link set dev {dev} '):
+if cmdline.startswith(f'ip link set dev {dev} '):
 
     os.system(f'ip route flush table {table}')
     assert 0 == os.system(f'ip route flush dev {dev}')
@@ -60,6 +62,8 @@ elif cmdline.startswith('ip route del '):
     assert 0 == os.system(f'ip route del {dst} table {table}')
 
 elif cmdline.startswith(f'ip addr add dev {dev}'):
+    # TEM QUE PEGAR O ENDEREÇO, RETIRAR O NETMASK, E ADICIONAR O ENDEREÇO /32
+    # E AI A PARTIR DESTE ENDEREÇO E DO NETMASK, OBTER A REDE, E ADICIONAR A ROTA PARA A TAL REDE USANDO A TABELA
 
     if '/' in cmdline:
         # ip addr add dev openvpn-0 10.8.8.8/24
@@ -87,9 +91,15 @@ elif cmdline.startswith(f'ip addr add dev {dev}'):
         # assert 0 == os.system(f'ip rule add from {ip}/32 table {table}')
     else:
         # ip addr add dev openvpn-200 local 10.211.1.145 peer 10.211.1.146
-        assert f' dev {dev}' in cmdline
 
-        os.execve('/sbin/ip', sys.argv, {})
+        _, _, _, _, _, local_, ip, peer_, peer = sys.argv
+
+        assert local_ == 'local'
+        assert peer_ == 'peer'
+
+        assert 0 == os.system(f'ip addr add {ip}/32 dev {dev}')
+        assert 0 == os.system(f'ip route add {peer} dev {dev} proto kernel scope link src {ip} table {table}')
+        #adicionar ele como default peer entao?
 
 elif cmdline.startswith(f'ip addr del dev {dev}'):
 
@@ -104,3 +114,8 @@ elif cmdline.startswith(f'ip addr del dev {dev}'):
     else:
         # ip addr del dev tun0 local 10.211.1.253 peer 10.211.1.254
         os.execve('/sbin/ip', sys.argv, {})
+
+else:
+    print('BAD COMMAND LINE')
+    print(cmdline)
+    os.exit(1)
